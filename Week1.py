73798716ef831e78279e6101ee2020c956e132a2
ASeveal <aseveal@hotmail.com>
2015-10-01 13:33:29 +0100

    Create Week1.py

---
+++ Week1.py
@@ -0,0 +1,17 @@
+Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:18:40) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("Hi there")
+Hi there
+>>> print ("Hi") print ("here")
+SyntaxError: invalid syntax
+>>> ================================ RESTART ================================
+>>> 
+hello
+there now
+>>> print ("and again")
+and again
+>>> ================================ RESTART ================================
+>>> 
+Hello world
+I am in my ISD class right now
+>>> 
